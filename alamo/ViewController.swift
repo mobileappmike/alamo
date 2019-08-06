ed53e80344a4ccfd46765155afbbbbd191327e04
Michael Miles <michael.gage.miles@gmail.com>
2019-08-06 01:50:17 -0700

    Initial Commit

---
+++ alamo/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  alamo
+//
+//  Created by Michael Miles on 8/6/19.
+//  Copyright © 2019 Michael Miles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var chuckLabel: UILabel!
+    @IBOutlet weak var pastebinTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func getQuotePressed(_ sender: Any) {
+    }
+    
+    @IBAction func pastebinButtonPressed(_ sender: Any) {
+    }
+    
+
+}
+
