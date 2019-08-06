afe2b8cb1fa487f668bb15963b22e4a6b97778bc
Michael Miles <michael.gage.miles@gmail.com>
2019-08-06 14:09:47 -0700

    Added Alamofire and SwiftyJSON pods, also created GET request for Chuck Norris quotes.

--- alamo/ViewController.swift
+++ alamo/ViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
     
@@ -19,6 +21,25 @@ class ViewController: UIViewController {
     }
     
     @IBAction func getQuotePressed(_ sender: Any) {
+        let url = "https://matchilling-chuck-norris-jokes-v1.p.rapidapi.com/jokes/random"
+        let apiKey = "aaec687b9amsh23f3e3c98b99be3p109206jsn31495e2b36f2"
+        
+        let header = ["X-RapidAPI-Key" : apiKey]
+        
+        Alamofire.request(url, method: .get, headers: header).responseJSON { (response) in
+            if response.result.isSuccess {
+                print("Got it! Chuck Norris is in the building.")
+                
+                if let chuckResponse = response.result.value {
+                    print("JSON: \(chuckResponse)")
+                    let chuckJSON : JSON = JSON(chuckResponse)
+                    self.chuckLabel.text = chuckJSON["value"].stringValue
+                }
+            } else {
+                print("Alamofire error: \(String(describing: response.result.error))")
+                self.chuckLabel.text = "Alamofire Issues"
+            }
+        }
     }
     
     @IBAction func pastebinButtonPressed(_ sender: Any) {
